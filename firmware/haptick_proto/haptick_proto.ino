bcb6759476ee6e5970860f667207f071a50a4094
Matthew Schubert <matthew.schubert@ioces.com>
2022-12-26 17:14:59 +0100

    Improve reliability of the prototype serial connection (#2)

--- firmware/haptick_proto/haptick_proto.ino
+++ firmware/haptick_proto/haptick_proto.ino
@@ -7,19 +7,25 @@ const int spi_ncs_pin = 10;
 // SPI settings
 const SPISettings spi_settings(8000000, MSBFIRST, SPI_MODE1);
 
+bool buffer_empty = true;
 char adc_buffer[24];
 
-void adc_data_ready() {
-  memset(adc_buffer, 0, 24);
-  SPI.beginTransaction(spi_settings);
-  digitalWrite(spi_ncs_pin, LOW);
-  SPI.transfer(adc_buffer, 24);
-  digitalWrite(spi_ncs_pin, HIGH);
-  SPI.endTransaction();
-  Serial.write(adc_buffer, 24);
+void adc_data_ready() 
+{
+  // If we've sent the last set of results, grab new ones into the buffer
+  if (buffer_empty)
+  {
+    SPI.beginTransaction(spi_settings);
+    digitalWrite(spi_ncs_pin, LOW);
+    SPI.transfer(adc_buffer, 24);
+    digitalWrite(spi_ncs_pin, HIGH);
+    SPI.endTransaction();
+    buffer_empty = false;
+  }
 }
 
-void setup()   {
+void setup()
+{
   // Create an 8 MHz 50% duty cycle clock for the ADC
   // This requires overclocking the Teensy to 96 MHz to
   // get a 48 MHz PWM clock, which is then divided by 6
@@ -56,18 +62,20 @@ void setup()   {
   digitalWrite(spi_ncs_pin, HIGH);
   SPI.endTransaction();
 
+  // Wait for serial to come up
+  while (!Serial);
+
   // Set up an interrupt on the ADC_!DRDY pin
   attachInterrupt(digitalPinToInterrupt(adc_ndrdy_pin), adc_data_ready, FALLING);
 }
 
 void loop()                     
 {
-  /*int32_t channel_1 = 0;
-  channel_1 |= adc_buffer[6] << 24;
-  channel_1 |= adc_buffer[7] << 16;
-  channel_1 |= adc_buffer[8] << 8;
-  channel_1 = channel_1 >> 8;
-  Serial.println(channel_1);
-  delay(50);*/
-  delay(500);
+  // Dump values out the serial port every time new readings come in
+  if (!buffer_empty)
+  {
+    Serial.write(adc_buffer, 24);
+    memset(adc_buffer, 0, 24);
+    buffer_empty = true;
+  }
 }
